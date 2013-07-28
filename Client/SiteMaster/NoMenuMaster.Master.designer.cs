1b850fb1848dab2369981697d07481404cd81a3a
Faizan S <faizan.shaharyar@gmail.com>
2013-07-28 06:28:59 +0200

    heavy development on the frontend. everything is now consolidated into the main.css file. please install and use web workbench for SASS compilation

--- Client/SiteMaster/NoMenuMaster.Master.designer.cs
+++ Client/SiteMaster/NoMenuMaster.Master.designer.cs
@@ -13,42 +13,6 @@ namespace Client.SiteMaster {
     public partial class NoMenuMaster {
         
         /// <summary>
-        /// Link1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlLink Link1;
-        
-        /// <summary>
-        /// Link2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlLink Link2;
-        
-        /// <summary>
-        /// Link3 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlLink Link3;
-        
-        /// <summary>
-        /// Link4 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlLink Link4;
-        
-        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
